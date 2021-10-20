1ec2f534de56d4133447be21a9166c8e6fbdda72
jaacob <jcbssmall@gmail.com>
2021-10-20 13:40:29 -0500

    Update ERC1155_IPFS.sol

--- ERC1155_IPFS.sol
+++ ERC1155_IPFS.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -6,32 +7,19 @@ import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract ERC1155IPFS is ERC1155Supply, Ownable {
-    using Strings for string;
-    using SafeMath for uint256;
-    
+contract ERC1155 is ERC1155Supply, Ownable {
+  using Strings for string;
+  using SafeMath for uint256;
 
-    uint256 private _currentTokenID = 0;
-    mapping (uint256 => address) public creators;
-    mapping (uint256 => uint256) public tokenSupply;
-    mapping (uint256 => string) public tokenUris;
+  uint256 private _currentTokenID = 0;
+  mapping (uint256 => string) public tokenURIs;
 
-    // Contract name
-    string public name;
-    // Contract symbol
-     string public symbol;
-    // Base metadata uri
-    string public baseURI;
-    // Conntract uri
-    string public _contractURI;
-
-  /**
-   * @dev Require msg.sender to be the creator of the token id
-   */
-  modifier creatorOnly(uint256 _id) {
-    require(creators[_id] == msg.sender, "ERC1155: ONLY_CREATOR_ALLOWED");
-    _;
-  }
+  // Contract name
+  string public name;
+  // Contract symbol
+  string public symbol;
+  // Base metadata uri
+  string public baseURI;
 
   constructor(
     string memory _name,
@@ -47,14 +35,13 @@ contract ERC1155IPFS is ERC1155Supply, Ownable {
     uint256 _id
   ) public view override returns (string memory) {
     require(exists(_id), "ERC1155: NONEXISTENT_TOKEN");
-    return string(abi.encodePacked(baseURI, tokenUris[_id]));
+    return string(abi.encodePacked(baseURI, tokenURIs[_id]));
   }
-
 
   /**
    * @dev Will update the base URL of token's URI
    * @param _baseURI New base URL of token's URI
-   */
+  */
   function setBaseURI(
     string memory _baseURI
   ) public onlyOwner {
@@ -66,28 +53,24 @@ contract ERC1155IPFS is ERC1155Supply, Ownable {
     * NOTE: remove onlyOwner if you want third parties to create new tokens on your contract (which may change your IDs)
     * @param _initialOwner address of the first owner of the token
     * @param _initialSupply amount to supply the first owner
-    * @param _uri Optional URI for this token type
-    * @param _data Data to pass if receiver is contract
+    * @param _uri URI for this token type
     * @return The newly created token ID
-    */
+  */
   function create(
     address _initialOwner,
     uint256 _initialSupply,
-    string calldata _uri,
-    bytes calldata _data
+    string calldata _uri
   ) external onlyOwner returns (uint256) {
 
-    uint256 _id = _getNextTokenID();
-    _incrementTokenTypeId();
-    creators[_id] = msg.sender;
+    uint256 _id = _currentTokenID.add(1);
+    _currentTokenID++;
 
     if (bytes(_uri).length > 0) {
       emit URI(_uri, _id);
     }
 
-    _mint(_initialOwner, _id, _initialSupply, _data);
-    tokenUris[_id] = _uri;
-    tokenSupply[_id] = _initialSupply;
+    _mint(_initialOwner, _id, _initialSupply, "0x00");
+    tokenURIs[_id] = _uri;
     return _id;
   }
 
@@ -96,16 +79,13 @@ contract ERC1155IPFS is ERC1155Supply, Ownable {
     * @param _to          Address of the future owner of the token
     * @param _id          Token ID to mint
     * @param _quantity    Amount of tokens to mint
-    * @param _data        Data to pass if receiver is contract
-    */
+  */
   function mint(
     address _to,
     uint256 _id,
-    uint256 _quantity,
-    bytes memory _data
-  ) public creatorOnly(_id) {
-    _mint(_to, _id, _quantity, _data);
-    tokenSupply[_id] = tokenSupply[_id].add(_quantity);
+    uint256 _quantity
+  ) external onlyOwner {
+    _mint(_to, _id, _quantity, "0x00");
   }
 
   /**
@@ -113,61 +93,25 @@ contract ERC1155IPFS is ERC1155Supply, Ownable {
     * @param _to          The address to mint tokens to
     * @param _ids         Array of ids to mint
     * @param _quantities  Array of amounts of tokens to mint per id
-    * @param _data        Data to pass if receiver is contract
-    */
+  */
   function mintBatch(
     address _to,
     uint256[] memory _ids,
-    uint256[] memory _quantities,
-    bytes memory _data
-  ) public {
-    for (uint256 i = 0; i < _ids.length; i++) {
-      uint256 _id = _ids[i];
-      require(creators[_id] == msg.sender, "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
-      uint256 quantity = _quantities[i];
-      tokenSupply[_id] = tokenSupply[_id].add(quantity);
-    }
-    _mintBatch(_to, _ids, _quantities, _data);
+    uint256[] memory _quantities
+  ) external onlyOwner {
+    _mintBatch(_to, _ids, _quantities, "0x00");
+  }
+  
+  /**
+   * @dev Update a token's URI at any point
+   * @param _id          Token ID to update URI
+   * @param _uri         URI for this token type
+  **/
+  function setURI(
+    uint256 _id,
+    string calldata _uri
+  ) external onlyOwner {
+    tokenURIs[_id] = _uri;
   }
 
-  /**
-    * @dev Change the creator address for given tokens
-    * @param _to   Address of the new creator
-    * @param _ids  Array of Token IDs to change creator
-    */
-  function setCreator(
-    address _to,
-    uint256[] memory _ids
-  ) public {
-    require(_to != address(0), "ERC1155Tradable#setCreator: INVALID_ADDRESS.");
-    for (uint256 i = 0; i < _ids.length; i++) {
-      uint256 id = _ids[i];
-      _setCreator(_to, id);
-    }
-  }
-
-  /**
-    * @dev Change the creator address for given token
-    * @param _to   Address of the new creator
-    * @param _id  Token IDs to change creator of
-    */
-  function _setCreator(address _to, uint256 _id) internal creatorOnly(_id)
-  {
-      creators[_id] = _to;
-  }
-
-  /**
-    * @dev calculates the next token ID based on value of _currentTokenID
-    * @return uint256 for the next token ID
-    */
-  function _getNextTokenID() private view returns (uint256) {
-    return _currentTokenID.add(1);
-  }
-
-  /**
-    * @dev increments the value of _currentTokenID
-    */
-  function _incrementTokenTypeId() private  {
-    _currentTokenID++;
-  }
 }
